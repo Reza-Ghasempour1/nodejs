738506afa4a2745ea6ba3e44fd5fc51d8f448f7a
Reza-Ghasempour1 <140710498+Reza-Ghasempour1@users.noreply.github.com>
2024-01-01 16:08:30 +0000

    implement server and client

---
+++ client/src/Form.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+const Form = () => {
+    const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    });
+    const handleInputChange = (e) => {
+        setFormData({
+        ...formData,
+        [e.target.name]: e.target.value,
+        });
+    };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch('https://reimagined-garbanzo-97jqq65x56wc7j5p-3001.app.github.dev/submitForm', {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+            });
+            const data = await response.json();
+            console.log('Server response:', data);
+            } catch (error) {
+                console.error('Error submitting form:', error);
+            }
+        };
+        return (
+        <div>
+        <form onSubmit={handleSubmit}>
+        <label>
+        Name:
+        <input
+        type="text"
+        name="name"
+        value={formData.name}
+        onChange={handleInputChange}
+        />
+        </label>
+        <br />
+        <label>
+        E-mail:
+        <input
+        type="text"
+        name="email"
+        value={formData.email}
+        onChange={handleInputChange}
+        />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+        </form>
+        </div>
+        );
+    };
+    export default Form;
